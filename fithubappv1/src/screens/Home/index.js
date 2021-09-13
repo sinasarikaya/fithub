7a623e09e3c35f91d012f0810ce9257a0569124b
sinasarikaya <msina_3453@hotmail.com>
2021-09-13 14:02:27 +0300

    fithubappv1 guncelleme

---
+++ fithubappv1/src/screens/Home/index.js
@@ -0,0 +1,151 @@
+import {withNavigation} from 'react-navigation';
+import {useNavigation} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import {navigationRef} from './RootNavigation';
+import {Component} from 'react';
+import React, {useState} from 'react';
+import type {Node} from 'react';
+import auth from '@react-native-firebase/auth';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+  Alert,
+  TextInput,
+  Image,
+  Navigator,
+  TouchableOpacity,
+  AsyncStorage,
+} from 'react-native';
+
+const Home = props => {
+  const {navigation} = props;
+  const [email, setUserEmail] = useState('');
+  const [password, setUserPassword] = useState('');
+
+  const setMail = text => setUserEmail(text);
+  const setPassword = text => setUserPassword(text);
+
+  const loginUser = async () => {
+    try {
+      await auth().signInWithEmailAndPassword(email, password);
+
+      AsyncStorage.setItem('@USER_ID', auth().currentUser.uid);
+      props.navigation.navigate('Mainpage');
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Üzgünüz!', 'Kullanıcı adı veya şifre hatalı !');
+    }
+  };
+
+  return (
+    <View style={styles.head}>
+      <View style={styles.logo}>
+        <Image
+          resizeMode={'center'}
+          source={require('./fithub7.png')}
+          style={{width: 200, height: 160}}
+        />
+      </View>
+
+      <Text style={styles.header}>GİRİŞ</Text>
+
+      <Text style={styles.idLabel}>E-mail Adresi:</Text>
+      <TextInput
+        style={styles.inputId}
+        onChangeText={setMail}
+        keyboardType="email-address"
+      />
+
+      <Text style={styles.pwLabel}>Şifre:</Text>
+      <TextInput
+        style={styles.inputPw}
+        onChangeText={setPassword}
+        placeholder="********"
+        secureTextEntry={true}
+      />
+
+      <View style={styles.mainbuttons}>
+        <View style={styles.loginbutton}>
+          <Button title="Giriş" color="#b3d9ff" onPress={loginUser} />
+        </View>
+
+        <View style={styles.registrybutton}>
+          <Button
+            title="Kayıt Ol"
+            color="#b3d9ff"
+            onPress={() => navigation.navigate('Signin')}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  head: {
+    backgroundColor: '#ccccff',
+    flex: 1,
+  },
+
+  header: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 50,
+    marginTop: 50,
+    marginLeft: 130,
+  },
+
+  idLabel: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 15,
+    marginLeft: 30,
+  },
+
+  pwLabel: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginLeft: 30,
+  },
+
+  inputId: {
+    backgroundColor: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginLeft: 30,
+    marginRight: 50,
+  },
+
+  inputPw: {
+    backgroundColor: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginLeft: 30,
+    marginRight: 50,
+  },
+
+  mainbuttons: {
+    marginTop: 40,
+    marginLeft: 110,
+    marginRight: 130,
+  },
+
+  registrybutton: {
+    marginTop: 15,
+  },
+
+  logo: {
+    marginTop: 100,
+    marginLeft: 90,
+  },
+});
+
+export default Home;
---
+++ fithubappv1/src/screens/Signin/index.js
@@ -0,0 +1,199 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import React, {useState} from 'react';
+import auth from '@react-native-firebase/auth';
+import {errorResolve} from './errorFunction';
+import {
+  View,
+  Text,
+  TextSafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Button,
+  Alert,
+  TextInput,
+  Image,
+  useColorScheme,
+  Picker,
+} from 'react-native';
+
+const Signin = props => {
+  const [selectedValue, setSelectedValue] = useState('erkek');
+  const {navigation} = props;
+
+  const [usermail, setUserMail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [userPasswordRep, setUserPasswordRep] = useState('');
+
+  const mail = text => setUserMail(text);
+  const password = text => setUserPassword(text);
+  const passwordRep = text => setUserPasswordRep(text);
+
+  const signUser = async () => {
+    if (userPassword === userPasswordRep) {
+      try {
+        await auth().createUserWithEmailAndPassword(usermail, userPassword);
+        Alert.alert('Hoşgeldiniz', 'Kayıt Başarıyla tamamlandı.');
+        props.navigation.goBack();
+      } catch (error) {
+        console.log(error);
+        Alert.alert('Üzgünüz..', errorResolve(error.code));
+      }
+    } else Alert.alert('Üzgünüz..', 'Şifre hatalı!');
+  };
+
+  const goBackPage = () => {
+    props.navigation.navigate('Home');
+  };
+
+  return (
+    <View style={styles.head}>
+      <View style={styles.logo}>
+        <Image
+          resizeMode={'center'}
+          source={require('./fithub7.png')}
+          style={{width: 200, height: 160}}
+        />
+      </View>
+      <Text style={styles.header}>KAYIT OL</Text>
+
+      <Text style={styles.ridLabel}>E-mail Adresi:</Text>
+      <TextInput
+        style={styles.rinputId}
+        onChangeText={mail}
+        keyboardType="email-address"
+      />
+
+      <Text style={styles.rpwLabel}>Şifre:</Text>
+      <TextInput
+        style={styles.rinputPw}
+        placeholder="********"
+        secureTextEntry={true}
+        onChangeText={password}
+      />
+
+      <Text style={styles.rpwLabel2}>Şifre Tekrar Gir:</Text>
+      <TextInput
+        style={styles.rinputPw2}
+        placeholder="********"
+        secureTextEntry={true}
+        onChangeText={passwordRep}
+      />
+      <Text style={styles.rcombobox}>Cinsiyet:</Text>
+      <Picker
+        style={styles.rpicker}
+        selectedValue={selectedValue}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+        <Picker.Item label="Erkek" value="erkek" />
+        <Picker.Item label="Kadın" value="kadın" />
+      </Picker>
+
+      <View style={styles.rregisterbutton}>
+        <Button title="Kayıt Ol" color="#b3d9ff" onPress={signUser} />
+      </View>
+      <View style={styles.rgobackbutton}>
+        <Button
+          title="Geri Dön"
+          color="#b3d9ff"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  head: {
+    backgroundColor: '#ccccff',
+    flex: 1,
+  },
+  logo: {
+    marginTop: 70,
+    marginLeft: 90,
+  },
+  header: {
+    marginTop: 50,
+    marginLeft: 110,
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  ridLabel: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginLeft: 30,
+  },
+
+  rpwLabel: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginLeft: 30,
+  },
+
+  rpwLabel2: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginLeft: 30,
+  },
+  rinputId: {
+    backgroundColor: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginLeft: 30,
+    marginRight: 50,
+  },
+
+  rinputPw: {
+    backgroundColor: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginLeft: 30,
+    marginRight: 50,
+  },
+
+  rinputPw2: {
+    backgroundColor: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginLeft: 30,
+    marginRight: 50,
+  },
+  rcombobox: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginLeft: 30,
+  },
+  rpicker: {
+    color: 'white',
+    backgroundColor: 'yellow',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginLeft: 30,
+    marginRight: 50,
+  },
+  rdt: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 15,
+    marginLeft: 30,
+  },
+  rregisterbutton: {
+    marginTop: 20,
+    marginLeft: 110,
+    marginRight: 130,
+  },
+  rgobackbutton: {
+    marginTop: 10,
+    marginLeft: 110,
+    marginRight: 130,
+  },
+});
+export default Signin;
+
+//propslara bak
+//onpress bak
